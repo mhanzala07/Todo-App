47371bc465e52f94b6661841535bcc79778682a7
mhanzala07 <hanzalawaseem073@gmail.com>
2024-03-27 00:33:56 +0500

    modified

--- app.js
+++ app.js
@@ -8,10 +8,56 @@ function addTodo(){
     // console.log(todoInput.value);
     arr.push(todoInput.value);
     console.log(arr);
+
     for (let i = 0; i < arr.length; i++) {
-        ol.innerHTML += `<li>${arr[i]} </li>`
-        
+        ol.innerHTML += `<li>${arr[i]} 
+        <button onclick = "deleteTodo(${i} )" >delete</button>
+        <button onclick = "editTodo(${i} )">edit</button>
+        </li>`
     }
 
     todoInput.value = ''
+}
+
+
+
+function deleteTodo(index){
+    console.log('delete todo' , index);
+    arr.splice(index , 1);
+    ol.innerHTML = ''
+    for (let i = 0; i < arr.length; i++) {
+        ol.innerHTML += `<li>${arr[i]} 
+        <button onclick = "deleteTodo(${i} )" >delete</button>
+        <button onclick = "editTodo(${i} )">edit</button>
+        </li>`
+    }
+
+}
+
+function editTodo(index){
+    // console.log('edit todo' , index);
+    let updatedValue = prompt ('enter your value')
+    arr.splice(index , 1 , updatedValue)
+    // console.log(updatedValue);
+    ol.innerHTML = ''
+    for (let i = 0; i < arr.length; i++) {
+        ol.innerHTML += `<li>${arr[i]} 
+        <button onclick = "deleteTodo(${i} )" >delete</button>
+        <button onclick = "editTodo(${i} )">edit</button>
+        </li>`
+    }
+
+    
+    ol.innerHTML = ''
+    // console.log(todoInput.value);
+    arr.push(todoInput.value);
+    console.log(arr);
+
+    for (let i = 0; i < arr.length; i++) {
+        ol.innerHTML += `<li>${arr[i]} 
+        <button onclick = "deleteTodo(${i} )" >delete</button>
+        <button onclick = "editTodo(${i} )">edit</button>
+        </li>`
+    }
+
 }
